39c6e86322b4a4e421606eadb4565734a5252256
MayurSrivastav <mayursrivastav04@gmail.com>
2026-08-17 21:37:55 +0530

    Time: 8 ms (7.85%), Space: 46.5 MB (5.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,14 +1,12 @@
-import java.util.*;
-
 class Solution {
 
     public List<List<Integer>> combinationSum(int[] c, int t) {
 
         List<List<Integer>> ans = new ArrayList<>();
-
         ArrayList<Integer> cur = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
 
-        subset(c, t, 0, cur, ans);
+        subset(c, t, 0, cur, ans, set);
 
         return ans;
     }
@@ -18,23 +16,32 @@ class Solution {
             int t,
             int i,
             ArrayList<Integer> cur,
-            List<List<Integer>> ans) {
+            List<List<Integer>> ans,
+            Set<List<Integer>> set) {
 
         if (i == c.length || t < 0) {
             return;
         }
 
         if (t == 0) {
-            ans.add(new ArrayList<>(cur));
+
+            if (!set.contains(cur)) {
+
+                ans.add(new ArrayList<>(cur));
+                set.add(new ArrayList<>(cur));
+            }
+
             return;
         }
 
         cur.add(c[i]);
 
-        subset(c, t - c[i], i, cur, ans);
+        subset(c, t - c[i], i + 1, cur, ans, set);
+
+        subset(c, t - c[i], i, cur, ans, set);
 
         cur.remove(cur.size() - 1);
 
-        subset(c, t, i + 1, cur, ans);
+        subset(c, t, i + 1, cur, ans, set);
     }
 }
