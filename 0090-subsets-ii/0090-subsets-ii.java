195412d0afc167c5d28efa1c21ba896bd4661d0e
MayurSrivastav <mayursrivastav04@gmail.com>
2026-08-18 20:16:45 +0530

    Time: 2 ms (99.71%), Space: 45.2 MB (24.14%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,8 +1,6 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-
         Arrays.sort(nums);
-
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
 
@@ -11,21 +9,22 @@ class Solution {
         return ans;
     }
 
-    public void find(int i, int[] nums, List<Integer> list,List<List<Integer>> ans) {
+    public void find(int i, int[] nums, List<Integer> list,
+                     List<List<Integer>> ans) {
 
-        ans.add(new ArrayList<>(list));
-
-        for (int j = i; j < nums.length; j++) {
-
-            if (j > i && nums[j] == nums[j - 1]) {
-                continue;
-            }
-
-            list.add(nums[j]);
-
-            find(j + 1, nums, list, ans);
-
-            list.remove(list.size() - 1);
+        if (i == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
         }
+
+        list.add(nums[i]);
+        find(i + 1, nums, list, ans);
+
+        list.remove(list.size() - 1);
+
+        while(i+1<nums.length && nums[i]==nums[i+1]){
+            i++;
+        }
+        find(i + 1, nums, list, ans);
     }
 }
