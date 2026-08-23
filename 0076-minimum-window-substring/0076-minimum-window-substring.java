b18b13f94b5e69e61e96ebdbda7bdcbaae15bfa1
MayurSrivastav <mayursrivastav04@gmail.com>
2026-08-23 19:34:33 +0530

    Time: 20 ms (37.38%), Space: 47.1 MB (48.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> need = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            need.put(c, need.getOrDefault(c, 0) + 1);
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        HashMap<Character, Integer> window = new HashMap<>();
-        int i = 0;
-        int j = 0;
-        int formed = 0;
-        int required = t.length();
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
-        while (j < s.length()) {
-            char c = s.charAt(j);
-            window.put(c, window.getOrDefault(c, 0) + 1);
-            if (need.containsKey(c) && window.get(c) <= need.get(c)) {
-                formed++;
+        int unqC=map.size();
+        int str=0;
+        int e=0;
+        int siz = Integer.MAX_VALUE;
+        int start=-1;
+        while(e<s.length()){
+            char c = s.charAt(e);
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)-1);
+                if(map.get(c)==0){
+                    unqC--;
+                }   
             }
-            j++;
-            while (formed == required) {
-                if (j - i < minLen) {
-                    minLen = j - i;
-                    start = i;
+            while(unqC==0){
+                int len=e-str+1;
+                if(len<siz){
+                    siz=len;
+                    start=str;
                 }
-                char left = s.charAt(i);
-                window.put(left, window.get(left) - 1);
-                if (need.containsKey(left)
-                        && window.get(left) < need.get(left)) {
-                    formed--;
+                char a = s.charAt(str);
+                if(map.containsKey(a)){
+                    map.put(a,map.get(a)+1);
+                    if(map.get(a)>0){
+                        unqC++;
+                    }   
                 }
-                i++;
-            }
+                str++;
+            } 
+        e++; 
         }
-        if (minLen == Integer.MAX_VALUE) {
-            return "";
-        }
-        return s.substring(start, start + minLen);
+    if(start==-1){
+        return "";
+    }
+    return s.substring(start,start+siz);
     }
 }
