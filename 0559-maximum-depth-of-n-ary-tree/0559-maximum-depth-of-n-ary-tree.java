6ee69b4f174f229360f6d67a21057b3645671630
MayurSrivastav <mayursrivastav04@gmail.com>
2026-09-13 14:17:16 +0530

    Time: 4 ms (2.2%), Space: 46.5 MB (7.2%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -1,15 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
     public int maxDepth(Node root) {
-        if(root==null) return 0;
-
-        int maxDepth=0;
-
-        for(Node node : root.children) {
-            if(node!=null) {
-                maxDepth = Math.max(maxDepth,maxDepth(node));
+        List<List<Integer>> a = levelOrder(root);
+        int s = a.size();
+        return s;
+    }
+    public List<List<Integer>> levelOrder(Node root) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> curr = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        if(root == null) {
+            return ans;
+        }
+        queue.offer(root);
+        int size = 1;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            curr.add(node.val);
+            size--;
+            if(node.children != null) {
+                for(Node child : node.children) {
+                    if(child != null) {
+                        queue.offer(child);
+                    }
+                }
+            }
+            if(size == 0) {
+                ans.add(curr);
+                curr = new ArrayList<>();
+                size = queue.size();
             }
         }
-
-        return (1+maxDepth);
+        return ans;
     }
 }
