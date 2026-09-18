b5a7cb100e34b2079007fc29239f267e74e13617
MayurSrivastav <mayursrivastav04@gmail.com>
2026-09-18 11:37:35 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,30 +1,24 @@
-import java.util.*;
-
 class Solution {
-
-    int[] t = new int[101];
-
-    int solve(int[] nums, int i, int n) {
-
-        if (i >= n)
-            return 0;
-
-        if (t[i] != -1)
-            return t[i];
-
-        int steal = nums[i] + solve(nums, i + 2, n);
-
-        int skip = solve(nums, i + 1, n);
-
-        return t[i] = Math.max(steal, skip);
-    }
-
     public int rob(int[] nums) {
-
-        int n = nums.length;
-
-        Arrays.fill(t, -1);
-
-        return solve(nums, 0, n);
+        int n= nums.length;
+        int [] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return find(dp,nums,n-1);
+    }
+    public int find(int [] dp,int[] nums,int n){
+        if(n==0){
+            dp[n]=nums[n];
+            return dp[n];
+        }
+        if(n==-1){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int Pick = nums[n]+find(dp,nums,n-2);
+        int notPick = 0+ find(dp,nums,n-1);
+        dp[n]=Math.max(Pick,notPick);
+        return dp[n];
     }
 }
